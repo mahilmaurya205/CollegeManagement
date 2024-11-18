4a91d53105b5ee35c1dcd31ac113d91b68ca9656
mahilmaurya205 <mahilmaurya2005@gmail.com>
2024-11-18 19:40:08 +0530

    Update StudentSignUpModel.cs

--- CollegeManagement/Models/StudentSignUpModel.cs
+++ CollegeManagement/Models/StudentSignUpModel.cs
@@ -5,12 +5,12 @@
         public int SID { get; set; }  // Primary Key
         public string Name { get; set; }
         public string Email { get; set; }
-        public string MobileNo { get; set; }
-        public string Gender { get; set; }
+        public string? MobileNo { get; set; }
+        public string? Gender { get; set; }
         public string? Address1 { get; set; }
         public string? Address2 { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
     }
 }
