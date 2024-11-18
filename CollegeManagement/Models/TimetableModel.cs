a5bf0413bad6bd7f13645672c9aa5e8e5452e965
mahilmaurya205 <mahilmaurya2005@gmail.com>
2024-11-18 19:39:32 +0530

    Update TimetableModel.cs

--- CollegeManagement/Models/TimetableModel.cs
+++ CollegeManagement/Models/TimetableModel.cs
@@ -4,8 +4,8 @@
     {
         public int ClassID { get; set; }
         public int SubjectID { get; set; }
-        public string Day { get; set; }
-        public string TimeSlot { get; set; }
-        public string Room { get; set; }
+        public string? Day { get; set; }
+        public string? TimeSlot { get; set; }
+        public string? Room { get; set; }
     }
 }
